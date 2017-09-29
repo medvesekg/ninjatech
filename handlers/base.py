2ed78e227dee4d9ee99dae00766d25c224e8979e
medvesekg <gregor_medvesek@yahoo.com>
2017-09-29 11:20:23 +0200

    Added feature to create new topics

--- handlers/base.py
+++ handlers/base.py
@@ -1,7 +1,7 @@
 import os
 import jinja2
 import webapp2
-
+from google.appengine.api import users
 
 template_dir = os.path.join(os.path.dirname(__file__), "../templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -20,9 +20,20 @@ class BaseHandler(webapp2.RequestHandler):
         return self.write(self.render_str(template, **kw))
 
     def render_template(self, view_filename, params={}):
+
+        # Cookies
         piskotek = self.request.cookies.get('piskotek')
         if piskotek:
             params['piskotek'] = True
+
+        # Google login
+        user = users.get_current_user()
+        if user:
+            params['user'] = user
+            params['logout_url'] = users.create_logout_url('/')
+        else:
+            params['login_url'] = users.create_login_url('/')
+
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
 
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import webapp2
 
 from handlers.main import MainHandler
 from handlers.cookie import CookieHandler
+from handlers.topic import TopicAdd
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler, name="main-page"),
     webapp2.Route('/set-cookie', CookieHandler),
+    webapp2.Route('/topic/add', TopicAdd),
 ], debug=True)
